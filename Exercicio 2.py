8c1c8212f4f889d5979df1a35274d7f6051af811
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-04-15 16:00:20 -0300

    Create Exercicio 2.py

---
+++ Exercicio 2.py
@@ -0,0 +1,15 @@
+print("Peça ao usuário um número inteiro positivo, e calcule a soma de todos os números de"
+      "1 até N, usando while\n")
+
+N = int(input("Digite um número inteiro e positivo: \n"))
+soma = 0
+contador = 1
+
+if N > 0:
+    while contador <= N:
+        soma = soma + contador
+        contador += 1
+    print(f"A soma de todos os números de 1 até {N} é {soma}")
+
+else:
+    print("Inválido")
