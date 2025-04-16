0b106ce1abb0664b9305ce8512826ca255dd857c
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-04-16 16:18:15 -0300

    Create Exercicio 5.py

---
+++ Exercicio 5.py
@@ -0,0 +1,12 @@
+num_first = int(input("Digite o primeiro número: \n"))
+num_second = int(input("Digite o segundo número: \n"))
+
+print(f"Números primos entre {num_first} e {num_second}:")
+
+for numero in range(num_first, num_second + 1):
+    if numero > 1:
+        for i in range(2, numero):
+            if numero % i == 0:
+                break
+        else:
+            print(numero)
