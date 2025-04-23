e739f08c6dd88087d725bf3c4ba04f061afb7e23
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-04-22 22:35:02 -0300

    Update exercicio9.py

--- exercicio9.py
+++ exercicio9.py
@@ -1,8 +1,9 @@
+lista = []
 for num in range(1, 10001):
     soma_divisores = 0
     for i in range(1, num):
         if num % i == 0:
             soma_divisores += i
-
     if soma_divisores == num:
-        print(f"{num} é um número perfeito!")
+        lista.append(num)
+print(f'Os números perfeito de 1 a 10.000 são: {lista}')
