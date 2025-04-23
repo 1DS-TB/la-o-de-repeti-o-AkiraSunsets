16c4594d1816fd38dc7cd28e7944d4feb20d3d16
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-04-22 22:35:54 -0300

    Update exercicio8.py

--- exercicio8.py
+++ exercicio8.py
@@ -1,18 +1,14 @@
 divisor = int(input("Insira o número: "))
-
 n = 1
 serie_harmonica = []
 soma = 0
-
 if divisor < 0:
     print("INVALIDO")
-    
 else:
     while n <= divisor:
         serie_harmonica.append(f"1/{n}")
         serie = 1/n
         n +=1
         soma += serie
-        
     print(serie_harmonica)
-    print(f"A soma da série harmônica é igual a: {soma:.2f}")
+    print(f"Soma da série harmônica é: {soma:.2f}")
